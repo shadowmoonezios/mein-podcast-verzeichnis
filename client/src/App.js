689a78980bb3a4234c20f1da296de6125aeaf518
shadowmoonezios <agushifelde@hotmail.com>
2025-09-06 21:57:51 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mein Podcast Verzeichnis</h1>
+      <p>Hier könnt ihr nach euren Lieblings-Podcasts suchen!</p>
+    </div>
+  );
+};
+
+export default App;
